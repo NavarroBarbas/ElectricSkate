5891c2d7f3e53c6222a5db316f3bd018d31b0434
Andreu Julià lliteras <andreujulia@estudiante.edib.es>
2023-05-12 17:51:17 +0200

    Parte del mení de GESTION DE PATINETES (Andreu)

--- ElectricSkate/src/Patinete.java
+++ ElectricSkate/src/Patinete.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
 
 public class Patinete {
+	
+	public static void MenuGestionarPatinetes() {
+        Scanner scanner = new Scanner(System.in);
+        
+        int opcion = 0;
+        do {
+        	System.out.println();
+            System.out.println("GESTIONAR PATINETES");
+            System.out.println();
+            System.out.println("1. Añadir Patinete");
+            System.out.println("2. Realizar Alquiler");
+            System.out.println("3. Realizar Devolución");
+            System.out.println("4. Atrás\n");
 
-	//prueba commit
+            System.out.print("Elige una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch(opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    System.out.println("\nVolviendo atrás...\n");
+                    break;
+                default:
+                    System.out.println("\nOpción inválida, vuelva a intentarlo.\n");
+            }
+        } while (opcion != 6);
+    }
+	
 }
