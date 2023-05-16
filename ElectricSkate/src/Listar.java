511e44bd14806e681313d574cc1f8c8e4300b48d
Valentín Silva Andrade <valen.silva196@gmail.com>
2023-05-16 17:20:52 +0200

    Metodos actualizados

--- ElectricSkate/src/Listar.java
+++ ElectricSkate/src/Listar.java
@@ -1,9 +1,12 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
+import java.sql.*;
 
-public class Listar {
-    public static void MenuListar() {
+ 	 class Listar {
+	 private static Connection conn;
+
+    public static void MenuListar() throws SQLException {
         Scanner scanner = new Scanner(System.in);
         
         
@@ -15,21 +18,24 @@ public class Listar {
             System.out.println("3. Listar todos los usuarios");
             System.out.println("4. Listar patinetes alquilados");
             System.out.println("5. Listar patinetes no alquilados");
-            System.out.println("6. Atrás\n");
+            System.out.println("6. Atr s\n");
 
-            System.out.print("Elige una opción: ");
+            System.out.print("Elige una opci n: ");
             opcion = scanner.nextInt();
             scanner.nextLine();
             
             switch(opcion) {
                 case 1:
                     System.out.println("\nListando usuarios empleados...\n");
+                    listarEmpleados();
                     break;
                 case 2:
                     System.out.println("\nListando usuarios clientes...\n");
+                    listarClientes();
                     break;
                 case 3:
                     System.out.println("\nListando todos los usuarios...\n");
+                    listarTodosUsuarios();
                     break;
                 case 4:
                     System.out.println("\nListando patinetes alquilados...\n");
@@ -38,13 +44,62 @@ public class Listar {
                     System.out.println("\nListando patinetes no alquilados...\n");
                     break;
                 case 6:
-                    System.out.println("\nVolviendo atrás...\n");
+                    System.out.println("\nVolviendo atr s...\n");
                     break;
                 default:
-                    System.out.println("\nOpción inválida, vuelva a intentarlo.\n");
+                    System.out.println("\nOpci n inv lida, vuelva a intentarlo.\n");
             }
         } while (opcion != 6);
     }
+    
+    public static void listarEmpleados() throws SQLException {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM usuarios WHERE rol = 'administrador'");
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                System.out.println("Empleado: " + nombre);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+            throw e;
+        }
+    }
+    
+    public static void listarClientes() throws SQLException {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM usuarios WHERE rol = 'cliente'");
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                System.out.println("Cliente: " + nombre);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+            throw e;
+        }
+    }
+    
+    public static void listarTodosUsuarios() throws SQLException {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM usuarios");
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String rol = rs.getString("rol");
+                System.out.println("Usuario: " + nombre + " (" + rol + ")");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+            throw e;
+        }
+    }
+
+
+
     
     public static void guardarListado(String lista, String nomArchivo) {
 		File fs = null;
@@ -71,7 +126,7 @@ public class Listar {
 				fw.close();
 			}
 			
-			System.out.println("La lista ha sido guardada con éxito!\n");
+			System.out.println("La lista ha sido guardada con  xito!\n");
 			
 		} catch (Exception e) {
 			System.out.println("No se ha podido guardar el listado\n");
